7d40d9cb7c016ad38c460b2e184ee344be161ba8
Vrunda Nawal <nawal.vrunda@gmail.com>
2022-12-07 15:41:06 +0000

    connect with DB

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 //app use cors as a middleware
 app.use(cors());
 app.use(express.json());
+
+//connect with mongoose
+mongoose.connect(
+  "mongodb://localhost:27017/full-mern-stack-auth",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("DB Connected successfully!!");
+  }
+);
 
 app.post("/api/register", (req, res) => {
   console.log(req.body);
