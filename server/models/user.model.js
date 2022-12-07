ea7c8f59b77b30ed7d2f05189de686f662715071
Vrunda Nawal <nawal.vrunda@gmail.com>
2022-12-07 15:27:59 +0000

    Set up the user model using mongoose

---
+++ server/models/user.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+//create a user schema
+const User = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    quote: { type: String },
+  },
+  { collection: "user-data" }
+);
+
+//create a model
+const model = mongoose.model("UserData", User);
+
+module.exports = model;
