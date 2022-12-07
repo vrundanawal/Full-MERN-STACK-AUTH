c0678c5fa89bf964a312f3e42fc2a9dd51a3eeff
Vrunda Nawal <nawal.vrunda@gmail.com>
2022-12-07 13:15:09 +0000

    Initial front ens and server setup

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Register</h1>
+      <form action="">
+        <input type="text" placeholder="Name" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+      </form>
+    </div>
+  );
+}
+
+export default App;
